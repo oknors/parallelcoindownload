760d455c312ebc7242042323ce3e22ba737c5e28
marcetin <marcetin@gmail.com>
2021-01-31 03:29:03 +0100

    Init

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Video from '../components/Video.svelte';
+
+</script>
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
+
+<Video />
+
+<main>
+	<slot></slot>
+</main>
